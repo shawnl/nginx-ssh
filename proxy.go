5a27ce4d8ecc543d719b01e9869f893842e9e514
Shawn Landden <shawn@churchofgit.com>
2013-12-21 13:38:07 -0800

    handle non-recognition of protocol correctly

--- proxy.go
+++ proxy.go
@@ -15,6 +15,7 @@ func copyAndClose(w io.Writer, r io.ReadCloser) {
 }
 
 func handleConnection(c net.Conn) {
+	defer c.Close()
 	r := bufio.NewReader(c)
 	buf, err := r.Peek(4)
 	var d net.Conn
@@ -22,15 +23,16 @@ func handleConnection(c net.Conn) {
 		d, err = net.Dial("tcp", "localhost:22")
 	} else if bytes.Equal(buf[:2], []byte{0x16, 0x03}) && buf[3] >= 0x00 && buf[3] <= 0x03 {
 		d, err = net.Dial("tcp", "localhost:443")
+	} else {
+		fmt.Println(c.RemoteAddr, "Protocol not recognized")
+		return
 	}
 	if err != nil {
-		fmt.Println(err)
-		c.Close()
+		fmt.Println(c.RemoteAddr, err)
 		return
 	}
 	go copyAndClose(c, d)
 	io.Copy(d, r)
-	c.Close()
 }
 
 func main() {
