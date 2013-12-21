0c028ecd8a72ce3c09cfebfff78adc06512c93c3
Shawn Landden <shawn@churchofgit.com>
2013-12-20 21:32:05 -0800

    add go implementation of suid binary (not used)

---
+++ spawn-ssh-suid.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"os/user"
+	"strconv"
+	"syscall"
+)
+
+const (
+	from_user string = "www-data"
+	fd        int    = 3
+)
+
+func main() {
+	var e error
+	u := new(user.User)
+	u, e = user.Lookup(from_user)
+	if e != nil {
+		os.Exit(1)
+	}
+
+	uid, e := strconv.Atoi(u.Uid)
+	if e != nil {
+		os.Exit(1)
+	}
+
+	if os.Getuid() != uid {
+		os.Exit(1)
+	}
+
+	e = syscall.Dup2(fd, syscall.Stdin)
+	if e != nil {
+		os.Exit(1)
+	}
+	e = syscall.Dup2(fd, syscall.Stdout)
+	if e != nil {
+		os.Exit(1)
+	}
+	e = syscall.Dup2(fd, syscall.Stderr)
+	if e != nil {
+		os.Exit(1)
+	}
+	e = syscall.Close(fd)
+	if e != nil {
+		os.Exit(1)
+	}
+
+	syscall.Setuid(0)
+	_ = syscall.Exec("/usr/sbin/sshd", []string{"sshd", "-i"}, []string{})
+	os.Exit(1)
+}
